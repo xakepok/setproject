4f3bc27c15fdebcc561c8bbe97245edb43b63089
asharikov <asharikov@bmstu.ru>
2019-01-17 14:31:19 +0300

    First commit

--- mod_setproject.php
+++ mod_setproject.php
@@ -14,4 +14,35 @@ defined('_JEXEC') or die;
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
+function selectProject()
+{
+    $project = JFactory::getApplication()->input->getString('set_active_project', '');
+    $session = JFactory::getSession();
+    if (is_numeric($project))
+    {
+        $session->set("active_project", $project);
+        if ($project === 0)
+        {
+            $session->clear("active_project");
+        }
+    }
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query->select("`id`, `title`")->from("`#__prj_projects`")->order("`title`");
+    $projects = $db->setQuery($query)->loadObjectList();
+    $options = array();
+    $options[] = JHtml::_('select.option', '0', 'MOD_SETPROJECT_SELECT_ACTIVE_PROJECT');
+    foreach ($projects as $item) {
+        $options[] = JHtml::_('select.option', $item->id, $item->title);
+    }
+    $attribs = 'class="inputbox" style="width: 500px;" onchange="this.form.submit()"';
+    return JHtml::_('select.genericlist', $options, 'set_active_project', $attribs, 'value', 'text',  $session->get('active_project', '0'), null, true);
+}
+function getProject()
+{
+    $session = JFactory::getSession();
+    $project = $session->get('active_project');
+    $activeProject = $project ?? JText::sprintf('MOD_SETPROJECT_ACTIVE_PROJECT_NOT_SELECT');
+    return JText::sprintf('MOD_SETPROJECT_ACTIVE_PROJECT', $activeProject);
+}
 require ModuleHelper::getLayoutPath('mod_setproject', $params->get('layout', 'default'));
