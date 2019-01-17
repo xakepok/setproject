5dd9d429c5eb603ad4d4e8771370622e44ab4cad
asharikov <asharikov@bmstu.ru>
2019-01-17 13:28:06 +0300

    Initial commit

---
+++ mod_setproject.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package    setproject
+ *
+ * @author     Admin <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require ModuleHelper::getLayoutPath('mod_setproject', $params->get('layout', 'default'));
---
+++ tmpl/default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package    setproject
+ *
+ * @author     Admin <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+defined('_JEXEC') or die;
+
+// Access to module parameters
+$domain = $params->get('domain', 'https://www.joomla.org');
+?>
+
+<a href="<?php echo $domain; ?>">
+	<?php echo 'setproject'; ?>
+</a>
