10969472bbd25cd4e99f0f56e7f1df70bc99df99
asharikov <asharikov@bmstu.ru>
2019-01-17 15:19:43 +0300

    Form

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,7 +14,8 @@ JHtml::_('formbehavior.chosen', 'select');
 // Access to module parameters
 $domain = $params->get('domain', 'https://www.joomla.org');
 ?>
-<?php echo JText::sprintf('MOD_SETPROJECT_ACTIVE_PROJECT_IS');?>
+<?php echo JText::sprintf('MOD_SETPROJECT_ACTIVE_PROJECT_IS'); ?>
 &nbsp;
-<?php echo selectProject(); ?>
-
+<form action="" method="post" name="adminForm" id="adminForm">
+    <?php echo selectProject(); ?>
+</form>
